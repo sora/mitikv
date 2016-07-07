cc9388e33f857a7d7d260648e7859e160b50d908
aomtoku <aom@sfc.wide.ad.jp>
2016-07-07 11:56:46 +0900

    eth_cncap : disable tuser

--- boards/nfsume/rtl/eth_encap.v
+++ boards/nfsume/rtl/eth_encap.v
@@ -19,7 +19,7 @@ module eth_encap
 	input  wire        m_axis_tready,
 	output wire        m_axis_tvalid,
 	output reg  [63:0] m_axis_tdata,
-	output reg  [ 7:0] m_axis_tkeep,
+	output wire [ 7:0] m_axis_tkeep,
 	output wire        m_axis_tlast,
 	output wire        m_axis_tuser
 );
@@ -83,39 +83,43 @@ always @ (*) begin
 	endcase
 end
 
-always @ (*) begin
-	if (tx_state == TX_HDR) begin
-		case (tx_count)
-			default: m_axis_tkeep = 8'b1111_1111;
-		endcase
-	end else if (tx_state == TX_DATA) begin
-		m_axis_tkeep = 8'b1111_1111;//dout[73:66];
-	end
-end
+assign m_axis_tkeep = (tx_state == TX_HDR) ? 8'b1111_1111 : 8'b0000_0000;
+
+//always @ (*) begin
+//	if (tx_state == TX_HDR) begin
+//		case (tx_count)
+//			default: m_axis_tkeep = 8'b1111_1111;
+//		endcase
+//	end else if (tx_state == TX_DATA) begin
+//		m_axis_tkeep = 8'b1111_1111;//dout[73:66];
+//	end
+//end
 
 always @ (*) begin
 	m_axis_tdata = 64'd0;
 	if (tx_state == TX_HDR) begin
 		case (tx_count)
-			0: m_axis_tdata = 64'h11_00_d1_91_5d_ba_e2_90;
-			1: m_axis_tdata = 64'h00_45_00_08_55_44_33_22;
-			2: m_axis_tdata = 64'h11_40_00_00_00_00_2e_00;
-			3: m_axis_tdata = 64'ha8_c0_01_64_a8_c0_62_31;
-			4: m_axis_tdata = 64'h1a_00_76_37_76_37_0b_64;
-			5: m_axis_tdata = 64'h3d_00_00_00_00_20_00_00;
-			6: m_axis_tdata = 64'hFF_FF_FF_FF_FF_FF_FF_FF;
-			7: m_axis_tdata = 64'h00_40_00_C0_00_00_00_00;
-			8: m_axis_tdata = 64'h01_08_00_00_00_00_70_00;
-			9: m_axis_tdata = 64'haa_aa_aa_aa_00_00_00_00;
+			16'h0: m_axis_tdata = 64'h11_00_d1_91_5d_ba_e2_90;
+			16'h1: m_axis_tdata = 64'h00_45_00_08_55_44_33_22;
+			16'h2: m_axis_tdata = 64'h11_40_00_00_00_00_2e_00;
+			16'h3: m_axis_tdata = 64'ha8_c0_01_64_a8_c0_62_31;
+			16'h4: m_axis_tdata = 64'h1a_00_76_37_76_37_0b_64;
+			16'h5: m_axis_tdata = 64'h3d_00_00_00_00_20_00_00;
+			16'h6: m_axis_tdata = 64'hFF_FF_FF_FF_FF_FF_FF_FF;
+			16'h7: m_axis_tdata = 64'h00_40_00_C0_00_00_00_00;
+			16'h8: m_axis_tdata = 64'h01_08_00_00_00_00_70_00;
+			16'h9: m_axis_tdata = 64'haa_aa_aa_aa_00_00_00_00;
 			default: m_axis_tdata = 64'h22222222_00000000;
 		endcase
-	end else if (tx_state == TX_DATA) begin
+	end 
+	/*
+	else if (tx_state == TX_DATA) begin
 		m_axis_tdata = 64'hf3ffffff_ffffffff;
-	end
+	end*/
 end
 
 assign m_axis_tlast  = (tx_state == TX_HDR && tx_count == 9);
-assign m_axis_tuser  = 1'b1;
+assign m_axis_tuser  = 1'b0;
 assign m_axis_tvalid = (tx_state == TX_HDR || tx_state == TX_DATA);
 
 /*
